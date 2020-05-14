8e44fe9aa78d63ab77b75dd80b8cae363737d229
mpgiii <georgariou3@gmail.com>
2020-05-14 13:15:54 -0700

    Add keypad support to switch between functionalities displayed

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include "lcd.h"
 #include "delay.h"
 #include "raindrops.h"
+#include "keypad.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define DEGREE_CHARACTER 223
+#define LOOP_DELAY 1000
+#define DHT_DELAY 1000000
 
 /**
  * main.c
@@ -17,6 +20,10 @@ void main(void) {
 
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
+	char key;
+	char current = 0xFF;
+	uint16_t DHT_count = 0;
+
 	set_DCO(FREQ_12_MHZ);
 
 	DHT_init();
@@ -24,33 +31,50 @@ void main(void) {
 	RD_init();
 
 	LCD_turn_off_cursor();
+	keypad_init();
 
+	/* loop delays 1 ms each loops */
 	while(1) {
-//	    DHT_read_data();
-//	    if (DHT_check_checksum()) {
-//	        LCD_go_to_line(1);
-//
-//	        sprintf(buffer, "Humidity = %2d%%", dht_data[0]);
-//	        LCD_write_string(buffer);
-//	        LCD_go_to_line(2);
-//	        sprintf(buffer, "Temp = %3d %cC", dht_data[2], DEGREE_CHARACTER);
-//            LCD_write_string(buffer);
-//	    }
-//	    else {
-//	        LCD_go_to_line(1);
-//	        LCD_write_string("Got bad data I guess");
-//	    }
-//
-//	    DHT_reset_data();
-//	    delay_us(1000000);
-	    LCD_go_to_line(1);
-	    if(RD_read_digital()) {
-	        LCD_write_string("Rain detected!    ");
-	    }
-	    else {
-	        LCD_write_string("Rain not detected.");
-	    }
-	    delay_us(1000);
 
+	    key = keypad_get_key();
+
+	    /* if we have pressed a new key, clear the display. */
+	    if (current != key && key != 0xFF) {
+	        current = key;
+	        LCD_clear();
+	    }
+
+	    /* when 1 or 2 is pressed, switch to DHT data on screen*/
+	    if (current == '1' || current == '2') {
+            DHT_read_data();
+            if (DHT_check_checksum()) {
+                LCD_go_to_line(1);
+                if (current == '1') {
+                    sprintf(buffer, "Humidity = %2d%%", dht_data[0]);
+                }
+                else if (current == '2') {
+                    sprintf(buffer, "Temp = %3d %cC", dht_data[2], DEGREE_CHARACTER);
+                }
+                LCD_write_string(buffer);
+            }
+	    }
+
+	    else if (current == '3') {
+            LCD_go_to_line(1);
+            if(RD_read_digital()) {
+                LCD_write_string("Rain detected!    ");
+            }
+            else {
+                LCD_write_string("Rain not detected.");
+            }
+	    }
+
+	    else {
+	        LCD_go_to_line(1);
+            LCD_write_string("Waiting for");
+            LCD_go_to_line(2);
+            LCD_write_string("input...");
+	    }
+        delay_us(LOOP_DELAY);
 	}
 }
